627830d365489b11d147cd1f80faf75c305498fe
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2022-05-14 09:59:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const transferCryptoTestamentContract = new ethers.Contract(transTestaABI, transTestaADDRESS, provider.getSigner());
+
 class Contract {
 	let addressContract;
 	let addressOwner;
@@ -13,20 +17,24 @@ class Contract {
 	remain=getRemain;
 	const Beneficiaris= getBeneficiaris().split(` `);
 	getOwner(addressContract){
-		
+		const owner = transferCryptoTestamentContract.getOwner();
+       	 	return owner;		
 	}
 	getTime(addressContract){
 
 	}
 	getTotal(addressContract){
-
+		const total = transferCryptoTestamentContract.getMount();
+        	return total;
 	}
 	getRemain(addressContract){
-
+		const remain = transferCryptoTestamentContract.getRemain();
+        	return remain;
 	}
 	//String split by ' ', split it to beneficiaris
 	getBeneficiaris(addressContract){
-
+		 const lisBeneficiaries = transferCryptoTestamentContract.getBeneficiaris();
+        	return lisBeneficiaries;
 	}
 	rollUp(){
 
